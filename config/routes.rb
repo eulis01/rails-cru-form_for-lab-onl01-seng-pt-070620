2bd5f02be316db641b77471030e5d5adc8feed92
eulis01 <eulis01@gmail.com>
2020-11-18 19:48:43 -0500

    resouces artists was singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artist, :songs, :genres, only:[:index, :show, :create, :new, :update, :edit]
+  resources :artists, only:[:index, :show, :create, :new, :update, :edit]
+  resources :songs, only:[:index, :show, :create, :new, :update, :edit]
+  resources :genres, only:[:index, :show, :create, :new, :update, :edit]
 end
